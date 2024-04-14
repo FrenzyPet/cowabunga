774316a57c34b1190d7471570d5fe3d155c1fe63
frenzy <manpatbro@gmail.com>
2024-04-14 19:33:28 +0300

    setup testing environment and write first unit test

---
+++ src/shared/lib/classnames/classnames.test.ts
@@ -0,0 +1,27 @@
+import { cn } from './classnames'
+
+describe('classNames', () => {
+	test('with only first param', () => {
+		expect(cn('someClass')).toBe('someClass')
+	})
+
+	test('with additional class', () => {
+		const expectResult = 'someClass class1 class2'
+		expect(cn('someClass', {}, ['class1', 'class2'])).toBe(expectResult)
+	})
+
+	test('with mods', () => {
+		const expectResult = 'someClass class1 class2 hovered scrollable'
+		expect(cn('someClass', { hovered: true, scrollable: true }, ['class1', 'class2'])).toBe(expectResult)
+	})
+
+	test('with mods false', () => {
+		const expectResult = 'someClass class1 class2 hovered'
+		expect(cn('someClass', { hovered: true, scrollable: false }, ['class1', 'class2'])).toBe(expectResult)
+	})
+
+	test('with mods undefined', () => {
+		const expectResult = 'someClass class1 class2 hovered'
+		expect(cn('someClass', { hovered: true, scrollable: undefined }, ['class1', 'class2'])).toBe(expectResult)
+	})
+})
