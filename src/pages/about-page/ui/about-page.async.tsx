ebf3be0e00137c0d5f3768c454dc926e61ff39fc
frenzy <manpatbro@gmail.com>
2024-08-13 21:47:49 +0300

    fix linter

--- src/pages/about-page/ui/about-page.async.tsx
+++ src/pages/about-page/ui/about-page.async.tsx
@@ -3,7 +3,8 @@ import { lazy } from 'react'
 export const AboutPageAsync = lazy(
 	() =>
 		new Promise((resolve) => {
-			// @ts-ignore
+			// @ts-expect-error: Эта ошибка ожидается, потому что я так сделал специально
+			// eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
 			setTimeout(() => resolve(import('./about-page')), 1500)
 		}),
 )
--- src/pages/main-page/ui/main-page.async.tsx
+++ src/pages/main-page/ui/main-page.async.tsx
@@ -3,7 +3,8 @@ import { lazy } from 'react'
 export const MainPageAsync = lazy(
 	() =>
 		new Promise((resolve) => {
-			// @ts-ignore
+			// @ts-expect-error: Эта ошибка ожидается, потому что я так сделал специально
+			// eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
 			setTimeout(() => resolve(import('./main-page')), 1500)
 		}),
 )
