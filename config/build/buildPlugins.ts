bd51a8f1f3b131f3429a24e14e723b26b76a01ed
frenzy <manpatbro@gmail.com>
2024-08-13 21:40:35 +0300

    disable bundle analyzer for production build

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,21 +5,27 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 import { BuildOptions } from './types/config'
 
-export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] => [
-	new webpack.ProgressPlugin(),
+export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] => {
+	const plugins = [
+		new webpack.ProgressPlugin(),
 
-	new HTMLWebpackPlugin({
-		template: paths.html,
-	}),
+		new HTMLWebpackPlugin({
+			template: paths.html,
+		}),
 
-	new MiniCssExtractPlugin({
-		filename: 'css/[name].[contenthash:8].css',
-		chunkFilename: 'css/[name].[contenthash:8].css',
-	}),
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].[contenthash:8].css',
+			chunkFilename: 'css/[name].[contenthash:8].css',
+		}),
 
-	new webpack.DefinePlugin({
-		__IS_DEV__: JSON.stringify(isDev),
-	}),
+		new webpack.DefinePlugin({
+			__IS_DEV__: JSON.stringify(isDev),
+		}),
+	]
 
-	new BundleAnalyzerPlugin({ openAnalyzer: false }),
-]
+	if (isDev) {
+		plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }))
+	}
+
+	return plugins
+}
