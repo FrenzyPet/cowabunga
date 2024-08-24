105b0491553fcc840a9ae496d1b10176f87f776e
frenzy <manpatbro@gmail.com>
2024-08-25 00:49:19 +0300

    fix test

--- src/widgets/sidebar/ui/sidebar/sidebar.test.tsx
+++ src/widgets/sidebar/ui/sidebar/sidebar.test.tsx
@@ -1,15 +1,16 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
+import { componentRender } from 'shared/lib/tests/componentRender'
 
 import { Sidebar } from './sidebar'
 
 describe('Sidebar', () => {
 	test('render sidebar', () => {
-		render(<Sidebar />)
+		componentRender(<Sidebar />)
 		expect(screen.getByTestId('sidebar')).toBeInTheDocument()
 	})
 
 	test('toggle sidebar', () => {
-		render(<Sidebar />)
+		componentRender(<Sidebar />)
 		const toggleButton = screen.getByTestId('sidebar-toggle')
 		expect(screen.getByTestId('sidebar')).toBeInTheDocument()
 		fireEvent.click(toggleButton)
